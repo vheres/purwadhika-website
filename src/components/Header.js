98ada072ff9b0cf6e6c5d5c57fdbd4652a3cef7c
William <william.gunawan@live.com>
2018-11-06 14:53:22 +0700

    commit 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,17 +21,18 @@ class Header extends Component {
         this.toggle = this.toggle.bind(this);
         this.state = {
           isOpen: false,
-          MouseOverButton: false
+          MouseOverButton: false,
+          dropdownCSS: ''
         };
       }
 
       handleButtonOpen = () => {
-        this.setState({ MouseOverButton: true})
+        this.setState({ MouseOverButton: true, dropdownCSS: 'header-button-select' })
 
       }
     
       handleButtonClose = () => {
-        this.setState({ MouseOverButton: false })
+        this.setState({ MouseOverButton: false, dropdownCSS: '' })
  
          
       }
@@ -44,36 +45,54 @@ class Header extends Component {
     renderHeader() {
         return(
             <div>
-                <Navbar color="dark" light expand="sm" className="py-0" style={{'height': '50px'}}>
+                <Navbar color="dark" light expand="sm" className="bg-transparent" style={{'height': '50px'}}>
                 <NavbarBrand href="/">reactstrap</NavbarBrand>
                 <NavbarToggler onClick={this.toggle} />
                 <Collapse isOpen={this.state.isOpen} navbar>
                     <Nav className="ml-auto" navbar>
                     <NavItem>
-                        <NavLink href="/components/">Components</NavLink>
+                        <NavLink href="/components/" className="mx-1">Components</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                        <NavLink href="https://github.com/reactstrap/reactstrap" className="mx-1">GitHub</NavLink>
                     </NavItem>
-                    <Dropdown nav inNavbar isOpen={this.state.MouseOverButton}>
-                        <DropdownToggle nav caret onMouseEnter={ this.handleButtonOpen } onMouseLeave={ this.handleButtonClose } className="header-button" >
-                        Options
+                    <Dropdown nav inNavbar isOpen={this.state.MouseOverButton} className="mx-1">
+                        <DropdownToggle nav caret onMouseEnter={ this.handleButtonOpen } onMouseLeave={ this.handleButtonClose } className={`${this.state.dropdownCSS}`} >
+                        <span className="header-dropdown-text">Options</span>
                         </DropdownToggle>
-                        <DropdownMenu right onMouseEnter={ this.handleButtonOpen } onMouseLeave={ this.handleButtonClose }>
-                            <div className="container-fluid" style={{'width': '500px'}}>
+                        <DropdownMenu right onMouseEnter={ this.handleButtonOpen } onMouseLeave={ this.handleButtonClose } className="header-dropdown-container">
+                            <div className="container-fluid" style={{'width': '600px'}}>
                                 <div className="row">
                                     <div className="col">
-                                    tes
+                                        <div className="row">
+                                            Full-Time Courses
+                                        </div>
+                                        <div className="row">
+                                            course 1
+                                        </div>
                                     </div>
                                     <div className="col">
-                                    set
+                                        <div className="row">
+                                            Part-Time Courses
+                                        </div>
+                                        <div className="row">
+                                            part-time course 1
+                                        </div>
+                                    </div>
+                                    <div className="col">
+                                        <div className="row">
+                                            Part-Time Courses
+                                        </div>
+                                        <div className="row">
+                                            part-time course 1
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </DropdownMenu>
                     </Dropdown>
                     <NavItem>
-                        <Button outline color="success" href="/signin">Sign in</Button>
+                        <Button outline color="success" href="/signin" className="mx-1">Sign in</Button>
                     </NavItem>
                     </Nav>
                 </Collapse>
